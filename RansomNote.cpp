3e67c2f6e2e1a9a9f8ad0980001f7d7e77ee5720
lyhcpp <lyhcpp@gmail.com>
2016-11-11 22:18:19 +0800

    git add testing

---
+++ RansomNote.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+bool canConstruct(string ransomNote, string magzazine){
+
+}
+
+int main() {
+    bool canConstruct(string ransomNote, string magazine);
+    string ransomNote{"asdfghjkl"},magazine{"asdfghjklzxcvbnm"};
+    return canConstruct(ransomNote, magazine);
+}
