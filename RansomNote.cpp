39de7789a143924811ae6ef45895a40d1c27e667
lyhcpp <lyhcpp@gmail.com>
2016-11-13 16:01:39 +0800

    Finish RansomNote, Use Hash

--- RansomNote.cpp
+++ RansomNote.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 bool canConstruct(string ransomNote, string magzazine){
-
+	vector<int> vecHash(256,0);
+	for(auto i:magzazine) vecHash[static_cast<int>(i)]++;
+	for(auto i:ransomNote) vecHash[static_cast<int>(i)]--;
+	for(auto &i:vecHash)
+		if (i<0) return false;
+	return true;
 }
 
 int main() {
     bool canConstruct(string ransomNote, string magazine);
-    string ransomNote{"asdfghjkl"},magazine{"asdfghjklzxcvbnm"};
-    return canConstruct(ransomNote, magazine);
+    string ransomNote("asdfghjkq"),magazine("asdfghjklzxcvbnm");
+    cout<<canConstruct(ransomNote, magazine)<<endl;
+    return 0;
 }
