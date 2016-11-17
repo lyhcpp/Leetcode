4d9c5a81c8e62a82478f25bf98640d6cfde52677
lyhcpp <lyhcpp@gmail.com>
2016-11-17 20:53:00 +0800

    Time完全版

--- C++Time.cpp
+++ C++Time.cpp
@@ -3,13 +3,15 @@
 #include<iostream>
 #include<time.h>
 
+using namespace std;
+
 int main(){
     clock_t start_time=clock();
     //被测试代码
     clock_t end_time=clock();
     cout<< "Running time is: "<<static_cast<double>(end_time-start_time)/CLOCKS_PER_SEC*1000<<"ms"<<endl;//输出运行时间
     return 0;
-} 
+}
 
 //clock_t,clock()定义于time.h中，clock（）返回从程序运行时刻开始的时钟周期数(类型为long)
 //CLOCKS_PER_SEC定义了每秒钟包含多少了时钟单元数，因为计算ms，所以*1000
